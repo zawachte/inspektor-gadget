zawachte/inspektor-gadget

commit 489e22c85ee1138cac3519f4536f3aba8b2333d3

Provide dockerfile for only CO-RE gadgets
Inspektor Gadget has two different kind of gadgets:
1: standard gadgets. These tools come from BCC, use Python and are
   compiled in the target machine before loading. They require to ship a
   whole compiling chain (clang) and libbcc. These tools have the
   advantage that they only require the kernel headers to be installed
   and work on systems without BTF support.
2: CO-RE gadgets: These tools use Compile Once - Run Everywhere. They
   are more lighter but require BTF info to be present.

A lot of distributions are enabling BTF and tools like BTFHub and BTFGen
make it possible to run those gadgets in systems without BTF. This
commit introduces a new dockerfile that provides support only for CO-RE
gadgets. The advantages of this approach are:
- The size of the image is reduced from ~800MB to ~200MB (when using
  alpine).
- A lot of things is removed from the container reducing the attack
  surface.
- The compilation is faster.
- Other images like fedora, debian, alpine can be used as base.

Makefile
@@ -35,7 +35,7 @@ LDFLAGS := "-X main.version=$(VERSION) \
 
 .DEFAULT_GOAL := build
 .PHONY: build
-build: manifests generate kubectl-gadget gadget-container
+build: manifests generate kubectl-gadget gadget-default-container
 
 .PHONY: all
 all: build local-gadget
@@ -112,10 +112,16 @@ install/kubectl-gadget: kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH)
 	mkdir -p ~/.local/bin/
 	cp kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH) ~/.local/bin/kubectl-gadget
 
-# gadget container
-.PHONY: gadget-container
-gadget-container:
-	docker build -t $(CONTAINER_REPO):$(IMAGE_TAG) -f gadget.Dockerfile \
+# gadget BCC container image
+.PHONY: gadget-default-container
+gadget-default-container:
+	docker build -t $(CONTAINER_REPO):$(IMAGE_TAG) -f gadget-default.Dockerfile \
+		--build-arg ENABLE_BTFGEN=$(ENABLE_BTFGEN) .
+
+# gadget CO-RE container image
+.PHONY: gadget-core-container
+gadget-core-container:
+	docker build -t $(CONTAINER_REPO):$(IMAGE_TAG) -f gadget-core.Dockerfile \
 		--build-arg ENABLE_BTFGEN=$(ENABLE_BTFGEN) .
 
 .PHONY: push-gadget-container

.github/workflows/inspektor-gadget.yml
@@ -193,8 +193,8 @@ jobs:
         name: ${{ matrix.local-gadget-target }}-tar-gz
         path: /home/runner/work/inspektor-gadget/inspektor-gadget/${{ matrix.local-gadget-target }}.tar.gz
 
-  build-docker-image:
-    name: Build gadget container image
+  build-gadget-default-container-image:
+    name: Build gadget default container image
     # level: 0
     runs-on: ubuntu-latest
     steps:
@@ -205,9 +205,9 @@ jobs:
       uses: actions/cache@v2
       with:
         path: /tmp/.buildx-cache
-        key: ${{ runner.os }}-single-buildx-${{ hashFiles('gadget.Dockerfile') }}
+        key: ${{ runner.os }}-docker-default-${{ hashFiles('gadget-default.Dockerfile') }}
         restore-keys: |
-          ${{ runner.os }}-single-buildx
+          ${{ runner.os }}-docker-default-
     - name: Login to Container Registry
       uses: docker/login-action@v1
       with:
@@ -225,17 +225,64 @@ jobs:
             IMAGE_TAG="latest"
         fi
         echo IMAGE_TAG=$IMAGE_TAG >> $GITHUB_ENV
-    - name: Build gadget container
+    - name: Build gadget default container
       uses: docker/build-push-action@v2
       with:
         context: /home/runner/work/inspektor-gadget/inspektor-gadget/
-        file: /home/runner/work/inspektor-gadget/inspektor-gadget/gadget.Dockerfile
+        file: /home/runner/work/inspektor-gadget/inspektor-gadget/gadget-default.Dockerfile
         build-args: |
           ENABLE_BTFGEN=true
         # TODO: how to avoid pushing a container before running integration tests
         # Answer: push to runner registry first, if integration tests are OK
         # push to final registry
         push: true
+        tags: ${{ secrets.CONTAINER_REPO }}:${{ env.IMAGE_TAG }}
+        cache-from: type=local,src=/tmp/.buildx-cache
+        cache-to: type=local,dest=/tmp/.buildx-cache-new
+
+  build-gadget-core-container-image:
+    name: Build gadget CO-RE container image
+    # level: 0
+    runs-on: ubuntu-latest
+    steps:
+    - name: Set up Docker Buildx
+      id: buildx
+      uses: docker/setup-buildx-action@v1
+    - name: Cache Docker layers
+      uses: actions/cache@v2
+      with:
+        path: /tmp/.buildx-cache
+        key: ${{ runner.os }}-docker-core-${{ hashFiles('gadget-core.Dockerfile') }}
+        restore-keys: |
+          ${{ runner.os }}-docker-core-
+    - name: Login to Container Registry
+      uses: docker/login-action@v1
+      with:
+        registry: ${{ secrets.CONTAINER_REGISTRY }}
+        username: ${{ secrets.CONTAINER_REGISTRY_USERNAME }}
+        password: ${{ secrets.CONTAINER_REGISTRY_PASSWORD }}
+    - name: Check out code
+      uses: actions/checkout@v1
+    - name: Set IMAGE_TAG
+      run: |
+        TMP1=${GITHUB_REF#*/}
+        TMP2=${TMP1#*/}
+        IMAGE_TAG=${TMP2//\//-}
+        if [ "$IMAGE_TAG" = "main" ]; then
+            IMAGE_TAG="latest"
+        fi
+        echo IMAGE_TAG=$IMAGE_TAG >> $GITHUB_ENV
+    - name: Build gadget CO-RE container
+      uses: docker/build-push-action@v2
+      with:
+        context: /home/runner/work/inspektor-gadget/inspektor-gadget/
+        file: /home/runner/work/inspektor-gadget/inspektor-gadget/gadget-core.Dockerfile
+        build-args: |
+          ENABLE_BTFGEN=true
+        # TODO: how to avoid pushing a container before running integration tests
+        # Answer: push to runner registry first, if integration tests are OK
+        # push to final registry
+        push: false # TODO: just build the image for the time being
         tags: ${{ secrets.CONTAINER_REPO }}:${{ env.IMAGE_TAG }}
         cache-from: type=local,src=/tmp/.buildx-cache
         cache-to: type=local,dest=/tmp/.buildx-cache-new
@@ -350,7 +397,7 @@ jobs:
   test-integration-aro:
     name: Integration tests on ARO
     # level: 1
-    needs: [check-secrets, test-unit, build-kubectl-gadget, build-local-gadget, build-docker-image]
+    needs: [check-secrets, test-unit, build-kubectl-gadget, build-local-gadget, build-gadget-default-container-image]
     # Run this job only if an ARO cluster is available on repo secrets. See
     # docs/ci.md for further details.
     if: needs.check-secrets.outputs.aro == 'true'
@@ -383,7 +430,7 @@ jobs:
   test-integration:
     name: Integration tests
     # level: 1
-    needs: [test-unit, build-kubectl-gadget, build-local-gadget, build-docker-image]
+    needs: [test-unit, build-kubectl-gadget, build-local-gadget, build-gadget-default-container-image]
     runs-on: ubuntu-latest
     steps:
     - name: Check out code

gadget-container/entrypoint.sh
@@ -47,7 +47,7 @@ echo $INSPEKTOR_GADGET_VERSION
 # mounted, passing /sys/fs/bpf from the pseudo-host does not work.
 # See also:
 # https://github.com/kubernetes/minikube/blob/99a0c91459f17ad8c83c80fc37a9ded41e34370c/deploy/kicbase/entrypoint#L76-L81
-BPF_MOUNTPOINT_TYPE="`stat -f --format=%T /sys/fs/bpf`"
+BPF_MOUNTPOINT_TYPE="`stat -f -c %T /sys/fs/bpf`"
 if [ "$BPF_MOUNTPOINT_TYPE" != "bpf_fs" ] ; then
   echo "/sys/fs/bpf is of type $BPF_MOUNTPOINT_TYPE. Remounting."
   mount -t bpf bpf /sys/fs/bpf/
@@ -59,7 +59,7 @@ if grep -q '^1:name=systemd:.*/crio-[0-9a-f]*\.scope$' /proc/self/cgroup > /dev/
     CRIO=1
 fi
 
-# In the gadget image, /usr/src is a symlink to /host/usr/src (see gadget.Dockerfile).
+# In the gadget image, /usr/src is a symlink to /host/usr/src (see gadget-*.Dockerfile).
 # If the kernel headers are already available on the gadget pod via the symlink,
 # no need to download them.
 if [ "$ID" = "rhcos" ] && [ ! -d "/usr/src/kernels/$KERNEL" ]; then
@@ -108,7 +108,7 @@ if [ "$ID" = "rhcos" ] && [ ! -d "/usr/src/kernels/$KERNEL" ]; then
       rpm2cpio $RPM | cpio --quiet -i
       popd
 
-      # In the gadget image, /usr/src is a symlink to /host/usr/src (see gadget.Dockerfile).
+      # In the gadget image, /usr/src is a symlink to /host/usr/src (see gadget-*.Dockerfile).
       # But in the case of RHCOS, remove the symlink and install files in the container.
       test ! -L /usr/src || rm -f /usr/src
       mkdir -p /usr/src/kernels/

gadget-core.Dockerfile
@@ -1,0 +1,95 @@
+# Dockerfile for Inspektor Gadget CO-RE container image gadgets.
+# This is a reduced gadget container image that supports only the CO-RE
+# implementation of the gadgets, i.e, it doesn't depend on BCC. This
+# image is much smaller than the image built with gadget.bcc.Dockerfile
+# and is designed to be used on systems that support BTF
+# (CONFIG_DEBUG_INFO_BTF).
+
+ARG BUILDER_IMAGE=ubuntu:20.04
+ARG BASE_IMAGE=alpine:3.14
+
+# Prepare and build gadget artifacts in a container
+FROM ${BUILDER_IMAGE} as builder
+
+ARG ENABLE_BTFGEN=false
+ENV ENABLE_BTFGEN=${ENABLE_BTFGEN}
+
+RUN set -ex; \
+	export DEBIAN_FRONTEND=noninteractive; \
+	apt-get update && \
+	apt-get install -y gcc make ca-certificates git clang \
+		software-properties-common libseccomp-dev llvm && \
+	add-apt-repository -y ppa:tuxinvader/kernel-build-tools && \
+	apt-add-repository -y ppa:longsleep/golang-backports && \
+	apt-get update && \
+	apt-get install -y libbpf-dev golang-1.17 && \
+	ln -s /usr/lib/go-1.17/bin/go /bin/go
+
+# Download BTFHub files
+COPY ./tools /btf-tools
+RUN set -ex; mkdir -p /tmp/btfs && \
+	if [ "$ENABLE_BTFGEN" = true ]; then \
+		cd /btf-tools && \
+		./getbtfhub.sh; \
+	fi
+
+# Cache go modules so they won't be downloaded at each build
+COPY go.mod go.sum /gadget/
+RUN cd /gadget && go mod download
+
+# This COPY is limited by .dockerignore
+COPY ./ /gadget
+RUN cd /gadget/gadget-container && make gadget-container-deps
+
+# Execute BTFGen
+RUN set -ex; \
+	if [ "$ENABLE_BTFGEN" = true ]; then \
+		cd /btf-tools && \
+		BTFHUB=/tmp/btfhub INSPEKTOR_GADGET=/gadget ./btfgen.sh; \
+	fi
+
+# Main gadget image
+FROM ${BASE_IMAGE}
+
+# install runtime dependencies  according to the package manager
+# available on the base image
+RUN set -ex; \
+	if command -v tdnf; then \
+		tdnf install -y libseccomp wget curl; \
+	elif command -v yum; then \
+		yum install -y libseccomp wget curl; \
+	elif command -v apt-get; then \
+		apt-get update && \
+		apt-get install -y seccompwget curl ; \
+	elif command -v apk; then \
+		apk add gcompat libseccomp bash wget curl ; \
+	fi && \
+	rmdir /usr/src || true && ln -sf /host/usr/src /usr/src && \
+	rm -f /etc/localtime && ln -sf /host/etc/localtime /etc/localtime
+
+COPY gadget-container/entrypoint.sh gadget-container/cleanup.sh /
+
+COPY --from=builder /gadget/gadget-container/bin/gadgettracermanager /bin/
+
+COPY gadget-container/gadgets/bcck8s /opt/bcck8s/
+
+## Hooks Begins
+
+# OCI
+COPY gadget-container/hooks/oci/prestart.sh gadget-container/hooks/oci/poststop.sh /opt/hooks/oci/
+COPY --from=builder /gadget/gadget-container/bin/ocihookgadget /opt/hooks/oci/
+
+# cri-o
+COPY gadget-container/hooks/crio/gadget-prestart.json gadget-container/hooks/crio/gadget-poststop.json /opt/hooks/crio/
+
+# nri
+COPY --from=builder /gadget/gadget-container/bin/nrigadget /opt/hooks/nri/
+COPY gadget-container/hooks/nri/conf.json /opt/hooks/nri/
+
+## Hooks Ends
+
+# BTF files
+COPY --from=builder /tmp/btfs /btfs/
+
+# Mitigate https://github.com/kubernetes/kubernetes/issues/106962.
+RUN rm -f /var/run

gadget-default.Dockerfile
@@ -1,5 +1,5 @@
 # Dockerfile for Inspektor Gadget container image.
-# This image contains CO-RE and BCC-based gadgets. It uses as base the
+# This image contains CO-RE and BCC-based gadgets. Its base image is the
 # BCC image. It's the default image that is deployed in Inspektor Gadget.
 
 ARG BUILDER_IMAGE=ubuntu:20.04
@@ -98,4 +98,4 @@ COPY gadget-container/hooks/nri/conf.json /opt/hooks/nri/
 COPY --from=builder /tmp/btfs /btfs/
 
 # Mitigate https://github.com/kubernetes/kubernetes/issues/106962.
-RUN rm /var/run
+RUN rm -f /var/run

commit dc494d20fa23e89956a79a687a602ae3dc111a4a

Makefile: Update minikube-install dependencies.
gadget-container was replaced by gadget-default-container and
gadget-core-container.
So, for minikube-install, we replace gadget-container by
gadget-default-container.

Fixes: 489e22c85ee1 ("Provide dockerfile for only CO-RE gadgets")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -171,7 +171,7 @@ generate-documentation:
 # minikube
 LIVENESS_PROBE_INITIAL_DELAY_SECONDS ?= 10
 .PHONY: minikube-install
-minikube-install: gadget-container kubectl-gadget
+minikube-install: gadget-default-container kubectl-gadget
 	# Unfortunately, minikube-cache and minikube-image have bugs in older
 	# versions. And new versions of minikube don't support all eBPF
 	# features. So we have to keep "docker-save|docker-load" for now.